9b4567f9445014cfca49c9baa2cdbe1cbbe4a41c
Jakkarohith <35620857+Jakkarohith@users.noreply.github.com>
2018-11-30 03:16:09 +0530

    Add files via upload

---
+++ week-3/Node js/IIFE/IIFEexample.js
@@ -0,0 +1,10 @@
+()=>{
+let firstname="jakka";
+let lastname="rohith";
+	(firstname,lastmame)=>
+	{
+	let age=30;
+	let height=5.11;
+	console.log("NAME :"+" "+firstname+" "+lastname+" "+"  AGE   :"+age+" HEIGHT :  "+height);	
+	}();
+}();
