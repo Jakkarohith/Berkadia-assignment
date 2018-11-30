612c5a6e3d0695ef8051112a374b480ee68bf011
Jakkarohith <35620857+Jakkarohith@users.noreply.github.com>
2018-11-30 09:01:12 +0530

    Add files via upload

---
+++ week-3/Node js/string functions/stringFunctionsExample.js
@@ -0,0 +1,64 @@
+//length
+var input = "anand reddy";
+var len = input.length;
+console.log(len)
+
+//indexOf
+var input1 = "hi hello im jakka rohith!!!";
+var pos1 = input1.indexOf("jakka");
+console.log(pos1);
+
+//lastIndeOf
+var input2 = "hi hello im jakka rohith!!!";
+var pos2 = input1.lastIndexOf("jakka");
+console.log(pos2);
+
+//search
+var input3 = "hi hello im jakka rohith!!!";
+var pos3 = input3.search("im");
+console.log(pos3);
+
+//slice
+var input4 = "hi hello im jakka rohith!!!";
+var res = input4.slice(7, 13);
+console.log(res);
+
+//substring
+var input5 = "hi hello im jakka rohith!!";
+var res1 = input5.substring(7, 13);
+console.log(res1);
+
+//replace
+var input6 = "hi hello im jakka rohith!!";
+var res3 = input6.replace("hello", "Jakka");
+console.log(res3);
+
+
+//trim
+
+var input7 = "      hi hello im jakka rohith!hi hello    im jakka rohith!hi hello i    m jakka rohith!     ";
+console.log(input7);
+
+//charAt
+
+var input7 = "hi hello im jakka rohith!! jakka rohith! jakka rohith! jakka rohith!";
+var res4= input6.charAt(12);
+console.log(res4);
+
+//toUppercase
+var input8 = "hi hello im jakka rohith!! jakka rohith! jakka rohith! jakka rohith!";
+var result=input8.toUpperCase;
+console.log(result);
+
+//toLowecase
+var input9 = "hi hello im JAKKKA jakka rohith!! jakka rohith! jakka rohith! jakka rohith!";
+var resultLower=input8.toLowerCase;
+console.log(resultLower);
+
+
+//charCodeAt
+
+var input8= " jakka rohith! jakka rohith! jakka rohith! jakka rohith! hi hello im jakka rohith!!";
+var res5= input6.charCodeAt(12);
+console.log(res5);
+
