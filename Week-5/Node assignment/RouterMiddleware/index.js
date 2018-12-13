5062928bd23d376d6fdbb534abf83513bae523bb
Jakkarohith <35620857+Jakkarohith@users.noreply.github.com>
2018-12-13 19:51:27 +0530

    Delete index.js

--- Week-5/Node assignment/RouterMiddleware/index.js
+++
@@ -1,7 +0,0 @@
-const express=require("express");
-const app=express();
-const routes=require("./routes");
-
-app.use("/ip",routes);
-
-app.listen(3000,()=>console.log("Listening on the port 3000"));
